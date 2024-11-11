678dfce9efa89437761f3902640fd77ff8eb340e
Matheus Pompeo <matheuspompeo13@gmail.com>
2024-11-11 09:38:11 -0300

    uml

--- Back/db/conn.js
+++ Back/db/conn.js
@@ -1,5 +1,5 @@
 const { Sequelize } = require('sequelize')
-const sequelize = new Sequelize('kanban', 'root', '1234', {
+const sequelize = new Sequelize('kanban', 'root', 'senai', {
     host: 'localhost',
     dialect: 'mysql'
 })
